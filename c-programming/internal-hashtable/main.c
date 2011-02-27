ab37dbcab07be8145ccab864c82b485ce8c0e8d4
Micha Hergarden <m_hergarden@gmx.net>
2011-02-27 13:26:50 +0100

    initial commit for internal hash
    Signed-off-by: Micha Hergarden <m_hergarden@gmx.net>

--- c-programming/internal-hashtable/main.c
+++ c-programming/internal-hashtable/main.c
@@ -10,9 +10,35 @@
 
 #include <mcheck.h>
 
-int main(int argc, char **argv) {
+/* Internal hashtable
+ *
+ * This is an example implementation of a so called 'closed hash'. The elements
+ * are placed within the hashtable itself, rather than referenced through linked
+ * lists.
+ */
 
-   mtrace();
+#define HASHSIZ 20
 
+struct item {
+   int val;
+};
+
+static struct item hashtable[HASHSIZ];
+
+int insert()
+{
    return 0;
 }
+
+int delete()
+{
+   return 0;
+}
+
+int lookup()
+{
+   return 0;
+}
+
+
+//   mtrace();
