d0ccc1ecca311793fa1dcfdf3142a4266b52843d
Micha Hergarden <micha.hergarden@gmail.com>
2013-07-23 09:35:00 +0200

    Added match for a fatal error. Also rearranged the code a bit to make it more readable.

--- python/ninja-color/ninja-color.py
+++ python/ninja-color/ninja-color.py
@@ -9,26 +9,33 @@ import time
 # It will take input on stdin or use a file if supplied.
 
 term = Terminal()
-errorline = re.compile('(: error:)');
-warningline = re.compile('(: warning:)');
-buildline = re.compile('^\[\d+/\d+\]');
+errorline      = re.compile('(: error:)');
+fatalerrorline = re.compile('(: fatal error:)')
+warningline    = re.compile('(: warning:)');
+buildline      = re.compile('^\[\d+/\d+\]');
 brokeOn = ""
 
 for line in fileinput.input():
 	line = line.decode('utf-8')
-	lineList = errorline.split(line)
-	if len(lineList) > 1:
-		print lineList[0] + term.bold + term.red + lineList[1] + term.normal + lineList[2],
-		brokeOn = line
-	else:
+
+	if errorline.search(line): 
+		lineList = errorline.split(line)
+		if len(lineList) == 3:
+			print lineList[0] + term.bold + term.red + lineList[1] + term.normal + lineList[2],
+			brokeOn = line
+	elif fatalerrorline.search(line):
+		lineList = fatalerrorline.split(line)
+		if len(lineList) == 3:
+			print lineList[0] + term.bold + term.red + lineList[1] + term.normal + lineList[2],
+			brokeOn = line
+	elif warningline.search(line):
 		lineList = warningline.split(line)
-		if len(warningline.split(line)) > 1:
+		if len(warningline.split(line)) == 3:
 			print lineList[0] + term.bold + term.yellow + lineList[1] + term.normal + lineList[2],
-		else:
-			if buildline.match(line):
-				print term.move_up + line,
-				#time.sleep(0.1) 
-			else:
-				print line,
+	elif buildline.search(line):
+			print term.move_up + line,
+			#time.sleep(0.1) 
+	else:
+		print line,
 
 print "\nYour build broke on the following:\n" + term.bold + brokeOn + term.normal
