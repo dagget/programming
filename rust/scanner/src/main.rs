50ebe9e8bf6f7cf264c1e63dba0d9bad9ef79387
Micha Hergarden <micha.hergarden@gmail.com>
2016-01-28 12:03:16 +0100

    Use clap for argument parsing

--- rust/scanner/src/main.rs
+++ rust/scanner/src/main.rs
@@ -1,56 +1,42 @@
-extern crate getopts;
+extern crate clap;
 
-use getopts::Options;
 use std::env;
 
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use clap::{Arg, App, SubCommand};
 
 fn collect_files(input: &str){
-	println!("using directory: {:?}", input);
-
 	let dir = Path::new(input);
 
 	if try!(fs::metadata(dir)).is_dir() {
-		for entry in try!(fs::read_dir(dir)) {
-			let entry = try!(entry);
-			if try!(fs::metadata(entry.path())).is_dir() {
-				//try!(visit_dirs(&entry.path(), cb));
-				println!("directory {:?}", entry.path);
-			} else {
-				println!("file {:?}", entry.path);
-			}
-		}
+//		for entry in try!(fs::read_dir(dir)) {
+//			let entry = try!(entry);
+//			if try!(fs::metadata(entry.path())).is_dir() {
+//				//try!(visit_dirs(&entry.path(), cb));
+//				println!("directory {:?}", entry.path);
+//			} else {
+//				println!("file {:?}", entry.path);
+//			}
+//		}
 	}
-	Ok(())
 }
-
-fn print_usage(program: &str, opts: Options) {
-	let brief = format!("Usage: {} [options]", program);
-	print!("{}", opts.usage(&brief));
-}
-
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let program = args[0].clone();
-	let mut opts = Options::new();
+	let matches = App::new("scanner")
+		.author("Micha Hergarden")
+		.about("My first rust app - a scanner/lexer")
+		.arg(Arg::with_name("SCANDIR")
+			.short("d")
+			.long("directory")
+			.help("The directory to scan")
+			.takes_value(true)
+			.required(true))
+		.get_matches();
 
-	opts.optflag("h", "help", "print help");
-	opts.optopt("d", "directory", "the directory containing input files.", "<directory>");
-
-	let matches = match opts.parse(&args[1..]) {
-		Ok(m) => { m }
-		Err(f) => { panic!(f.to_string()) }
-	};
-	if matches.opt_present("h") {
-		print_usage(&program, opts);
-		return;
-	}
-
-	let input = matches.opt_str("d");
-
+	let scandirectory = matches.value_of("SCANDIR").unwrap();
 	println!("start scan");
-	collect_files(input);
+	println!("using directory: {}", scandirectory);
+	collect_files(&scandirectory);
 }
