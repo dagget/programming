1e0a0595585c8db8bf1e8c533a721300b99f5a43
Micha Hergarden <micha.hergarden@gmail.com>
2016-06-24 21:51:44 +0200

    finished gigasecond

--- rust/gigasecond/tests/gigasecond.rs
+++ rust/gigasecond/tests/gigasecond.rs
@@ -22,28 +22,24 @@ fn test_date() {
 }
 
 #[test]
-#[ignore]
 fn test_another_date() {
     let start_date = UTC.ymd(1977, 6, 13).and_hms(0,0,0);
     assert_eq!(gigasecond::after(start_date), UTC.ymd(2009, 2, 19).and_hms(1,46,40));
 }
 
 #[test]
-#[ignore]
 fn test_third_date() {
     let start_date = UTC.ymd(1959, 7, 19).and_hms(0,0,0);
     assert_eq!(gigasecond::after(start_date), UTC.ymd(1991, 3, 27).and_hms(1,46,40));
 }
 
 #[test]
-#[ignore]
 fn test_datetime() {
     let start_date = UTC.ymd(2015, 1, 24).and_hms(22,0,0);
     assert_eq!(gigasecond::after(start_date), UTC.ymd(2046, 10, 2).and_hms(23,46,40));
 }
 
 #[test]
-#[ignore]
 fn test_another_datetime() {
     let start_date = UTC.ymd(2015, 1, 24).and_hms(23,59,59);
     assert_eq!(gigasecond::after(start_date), UTC.ymd(2046, 10, 3).and_hms(1,46,39));
