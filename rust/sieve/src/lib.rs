7d2ded99dcb788c21380f612713afa199d8579a7
Micha Hergarden <micha.hergarden@gmail.com>
2017-04-10 21:48:22 +0200

    added modified version (gotten from other solutions) as optimized version

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,19 +1,59 @@
+//#![feature(test)]
+//extern crate test;
+
+//pub struct Sieve;
+//impl Sieve {
+//	pub fn primes_up_to(max: usize) -> Vec<usize> {
+//		if max < 2 {
+//			return vec![]
+//		}
+//
+//		// Optimization could be done by leaving out even values,
+//		// but step_by is an unstable feature and so is the inclusive
+//        // range (...)
+//		let mut result: Vec<usize> = (2..max+1).collect();
+//
+//		for number in 2..max+1 {
+//			result.retain(|&x| x == number || x%number > 0);
+//		}
+//
+//		result.clone()
+//	}
+//}
 pub struct Sieve;
-impl Sieve {
-	pub fn primes_up_to(max: usize) -> Vec<usize> {
-		if max < 2 {
-			return vec![]
-		}
 
-		// Optimization could be done by leaving out even values,
-		// but step_by is an unstable feature and so is the inclusive
-        // range (...)
-		let mut result: Vec<usize> = (2..max+1).collect();
-
-		for number in 2..max+1 {
-			result.retain(|&x| x == number || x%number > 0);
-		}
-
-		result.clone()
-	}
+impl Sieve{
+    pub fn primes_up_to(max:usize) -> Vec<usize>{
+        let n = max+1;
+        let mut sieve = vec![true;n];
+        let sqn = f64::sqrt(n as f64) as usize;
+        (2..n).filter(|&i|{
+            if i <= sqn && sieve[i] {
+                let mut j = i+i;
+                while j < n {
+                    sieve[j]=false;
+                    j+=i;
+                }                    
+            };
+            sieve[i]
+        })
+        .collect()
+    }   
 }
+
+
+
+//#[cfg(test)]
+//mod tests {
+//    use super::*;
+//    use test::Bencher;
+//
+//    #[test]
+//    fn it_works() {
+//    }
+//
+//    #[bench]
+//    fn bench_sieve(b: &mut Bencher) {
+//        b.iter(|| Sieve::primes_up_to(10000));
+//    }
+//}
