3ff96ff2c7f47e0fcc4169e844f1b28b5ae09c1f
Micha Hergarden <micha.hergarden@gmail.com>
2019-12-24 10:03:47 +0100

    After clippy and fmt are applied.

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,4 +1,3 @@
-
 fn is_divisable_by_2(n: u64) -> bool {
     n & 0x1 == 0
 }
@@ -6,7 +5,7 @@ fn is_divisable_by_2(n: u64) -> bool {
 fn is_divisible_by_3(n: u64) -> bool {
     let mut sum: u64 = 0;
     let mut rem = n;
-    
+
     while rem > 0 {
         sum += rem % 10;
         rem /= 10;
@@ -21,25 +20,27 @@ fn is_divisible_by_5(n: u64) -> bool {
 
 fn is_divisible_by_7(n: u64) -> bool {
     let last_digit = n % 10;
-    let other_digits = n/10;
+    let other_digits = n / 10;
 
     other_digits.checked_sub(last_digit * 2).unwrap_or(n) % 7 == 0
 }
 
 fn is_prime(n: u64) -> bool {
-     for i in 2 .. n/2 {
-         if n % i == 0 {
-             return false;
-         }
-     }
-     true
+    for i in 2..n / 2 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
 }
 
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut factors : Vec<u64> = vec![];
+    let mut factors: Vec<u64> = vec![];
     let mut remainder: u64 = n;
 
     while remainder > 1 {
+        // try to reduce the remainder
+        // by applying divisibility rules.
         if is_divisable_by_2(remainder) {
             factors.push(2);
             remainder /= 2;
@@ -50,7 +51,7 @@ pub fn factors(n: u64) -> Vec<u64> {
             remainder /= 3;
             continue;
         }
-        if is_divisible_by_5(remainder){
+        if is_divisible_by_5(remainder) {
             factors.push(5);
             remainder /= 5;
             continue;
@@ -61,20 +62,21 @@ pub fn factors(n: u64) -> Vec<u64> {
             continue;
         }
 
+        // brute forcing is expensive, so
+        // 'quick' check for prime first
         if is_prime(remainder) {
             factors.push(remainder);
             break;
         }
 
         // no divisibility rule available
-        // use brute force
-        for divisor in 11 ..= remainder/2 {
-            if is_prime(divisor){
-                if remainder % divisor == 0 {
-                    factors.push(divisor);
-                    remainder /= divisor;
-                    break;
-                }
+        // use brute force: just try out all
+        // prime divisors.
+        for divisor in 11..=remainder / 2 {
+            if is_prime(divisor) && remainder % divisor == 0 {
+                factors.push(divisor);
+                remainder /= divisor;
+                break;
             }
         }
     }
