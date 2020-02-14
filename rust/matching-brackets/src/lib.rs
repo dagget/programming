ef3b2357344358159a62c135b91692aebfcbbfa8
Micha Hergarden <micha.hergarden@gmail.com>
2020-02-14 21:19:16 +0100

    Finished matching brackets.

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut stack: Vec<char> = vec![];
+
+    let mut result = 
+        string
+        .chars()
+        .map( |x| match x {
+            '(' => {stack.push('('); true},
+            '{' => {stack.push('{'); true},
+            '[' => {stack.push('['); true},
+            ']' => stack.pop() == Some('['),
+            '}' => stack.pop() == Some('{'),
+            ')' => stack.pop() == Some('('),
+            _ => true,
+        })
+        .all(|x| x);
+
+    result &= stack.is_empty();
+    result
+}
