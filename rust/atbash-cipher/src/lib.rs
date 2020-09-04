5cbc8cf115fab39d828e7b75b20237bd523d6fe0
Micha Hergarden <micha.hergarden@gmail.com>
2020-09-04 20:30:50 +0200

    Fixed atbash cipher by switching the implementations around and simplifying it. The unwrap was removed to prevent the application crashing unnecessarily.

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,30 +1,38 @@
-const ALPHABET : [char;26]  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const SZ : usize = ALPHABET.len() - 1;
+const ALPHABET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+const SZ: usize = ALPHABET.len() - 1;
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let mut encoded = plain.to_lowercase();
-    encoded = encoded.chars().filter(|x| x.is_alphanumeric()).collect();
-    encoded = encoded 
-             .chars()
-             .map(|x| if x.is_alphabetic() { ALPHABET[SZ - ALPHABET
-                                        .iter()
-                                        .position(|c| *c == x)
-                                        .unwrap()]
-                                      } else {
-                                          x
-                                      }
-                 )
-             .collect::<String>();
+    let mut i = 0;
 
-    let mut split_encoded = String::new();
-    while encoded.len() > 5 {
-        split_encoded += &(encoded.drain(..5).collect::<String>() + " ");
-    }
-    split_encoded + &encoded
+    decode(&plain.to_lowercase())
+        .chars()
+        .filter(|&x| x != '.' && x != ',')
+        .fold(String::new(), |mut encoded, x| {
+            if i == 5 {
+                encoded.push(' ');
+                i = 0;
+            }
+            encoded.push(x);
+            i += 1;
+            encoded
+        })
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    encode(cipher).chars().filter(|x| x.is_alphanumeric()).collect()
+    cipher
+        .chars()
+        .filter(|&x| !x.is_whitespace())
+        .map(|x| {
+            if let Some(i) = ALPHABET.iter().position(|c| *c == x) {
+                ALPHABET[SZ - i]
+            } else {
+                x
+            }
+        })
+        .collect::<String>()
 }
