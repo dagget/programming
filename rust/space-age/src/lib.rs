ea7fbcc09445ecb25e12732c6d40d2aebdcf91f7
Micha Hergarden <micha.hergarden@gmail.com>
2020-09-22 21:56:57 +0200

    reworked program to work towards a macro.

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,23 +1,25 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
+const SECONDS_IN_EARTH_YEAR: f64=(365.25 * 24.0 * 60.0 * 60.0);
 
 #[derive(Debug)]
 pub struct Duration {
-    value: u64,
+    earth_years: f64,
 }
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration { value: s }
+        Duration { 
+            earth_years: s as f64 / SECONDS_IN_EARTH_YEAR
+        }
     }
 }
 
 pub trait Planet {
+    const RELATIVE_PERIOD: f64;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.earth_years / Self::RELATIVE_PERIOD
     }
 }
 
@@ -31,42 +33,26 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 0.240_846_7
-    }
+        const RELATIVE_PERIOD: f64 = 0.240_846_7;
 }
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 0.615_197_26
-    }
+        const RELATIVE_PERIOD: f64 = 0.615_197_26;
 }
 impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.value as f64 / (365.25 * 24.0 * 60.0 * 60.0)
-    }
+        const RELATIVE_PERIOD: f64 = 1.0;
 }
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 1.880_815_8
-    }
+        const RELATIVE_PERIOD: f64 = 1.880_815_8;
 }
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 11.862_615
-    }
+        const RELATIVE_PERIOD: f64 = 11.862_615;
 }
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 29.447_498
-    }
+        const RELATIVE_PERIOD: f64 = 29.447_498;
 }
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 84.016_846
-    }
+        const RELATIVE_PERIOD: f64 = 84.016_846;
 }
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / 164.79132
-    }
+        const RELATIVE_PERIOD: f64 = 164.79132;
 }
