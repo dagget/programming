ad5a067ad4f31d7beeb2c6115b61aa2be7db9fea
Micha Hergarden <micha.hergarden@gmail.com>
2020-10-01 13:42:07 +0200

    Refactored to use a macro.

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,6 +1,6 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
-const SECONDS_IN_EARTH_YEAR: f64=(365.25 * 24.0 * 60.0 * 60.0);
+const SECONDS_IN_EARTH_YEAR: f64 = (365.25 * 24.0 * 60.0 * 60.0);
 
 #[derive(Debug)]
 pub struct Duration {
@@ -9,8 +9,8 @@ pub struct Duration {
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration { 
-            earth_years: s as f64 / SECONDS_IN_EARTH_YEAR
+        Duration {
+            earth_years: s as f64 / SECONDS_IN_EARTH_YEAR,
         }
     }
 }
@@ -23,36 +23,20 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! space_age {
+    ($name:ident, $factor:expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            const RELATIVE_PERIOD: f64 = $factor;
+        }
+    };
+}
 
-impl Planet for Mercury {
-        const RELATIVE_PERIOD: f64 = 0.240_846_7;
-}
-impl Planet for Venus {
-        const RELATIVE_PERIOD: f64 = 0.615_197_26;
-}
-impl Planet for Earth {
-        const RELATIVE_PERIOD: f64 = 1.0;
-}
-impl Planet for Mars {
-        const RELATIVE_PERIOD: f64 = 1.880_815_8;
-}
-impl Planet for Jupiter {
-        const RELATIVE_PERIOD: f64 = 11.862_615;
-}
-impl Planet for Saturn {
-        const RELATIVE_PERIOD: f64 = 29.447_498;
-}
-impl Planet for Uranus {
-        const RELATIVE_PERIOD: f64 = 84.016_846;
-}
-impl Planet for Neptune {
-        const RELATIVE_PERIOD: f64 = 164.79132;
-}
+space_age!(Mercury, 0.240_846_7);
+space_age!(Venus, 0.615_197_26);
+space_age!(Earth, 1.0);
+space_age!(Mars, 1.880_815_8);
+space_age!(Jupiter, 11.862_615);
+space_age!(Saturn, 29.447_498);
+space_age!(Uranus, 84.016_846);
+space_age!(Neptune, 164.79132);
