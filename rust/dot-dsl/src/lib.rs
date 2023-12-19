eea5c54d5c9adeab140448a38215a3fe6d2b6a94
Micha Hergarden <micha.hergarden@gmail.com>
2023-12-19 10:19:34 +0100

    Finished dot-dsl

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,51 +1,95 @@
+
+
 pub mod graph {
+    use graph_items::node::Node;
+    use graph_items::edge::Edge;
+    use std::collections::HashMap;
+
+    #[derive(Default)]
     pub struct Graph {
-        pub nodes : Vec<graph_items::node::Node>,
-        pub edges : Vec<graph_items::edge::Edge>,
-        pub attrs : Vec<(String, String)>
+        pub nodes : Vec<Node>,
+        pub edges : Vec<Edge>,
+        pub attrs : HashMap<String,String>,
     }
 
     impl Graph {
         pub fn new() -> Self {
-            Graph {
-                nodes : vec![],
-                edges : vec![],
-                attrs : vec![]
-            }
+            Self::default()
         }
 
-        pub fn with_nodes(self, nodes: &Vec<graph_items::node::Node>) -> Self {
-            Graph {
-                nodes : nodes.to_vec(),
-                edges : self.edges,
-                attrs : self.attrs
-            }
+        pub fn with_nodes(mut self, nodes: &Vec<Node>) -> Self {
+            self.nodes = nodes.to_vec();
+            self
         }
+
+        pub fn with_edges(mut self, edges: &Vec<Edge>) -> Self {
+            self.edges = edges.to_vec();
+            self
+        }
+
+        pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+            self.attrs = attrs.iter().map(|(key, value)| (key.to_string(), value.to_string())).collect();
+            self
+        }
+
+        pub fn get_node(self, name: &str ) -> Option<Node> {
+            self.nodes.into_iter().find(|x| x.label == name)
+        }
+
     }
 
     pub mod graph_items {
+
         pub mod edge {
-            pub struct Edge;
+            use std::collections::HashMap;
+
+            #[derive(PartialEq,Clone,Debug)]
+            pub struct Edge {
+                pub start : String,
+                pub end : String,
+                pub attrs : HashMap<String, String>,
+            }
+
+            impl Edge {
+                pub fn new(start: &str, end: &str) -> Self {
+                    Edge{
+                        start: start.to_string(),
+                        end: end.to_string(),
+                        attrs: HashMap::new(),
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    self.attrs = attrs.iter().map(|(key, value)| (key.to_string(), value.to_string())).collect();
+                    self
+                }
+            }
         }
 
         pub mod node {
+            use std::collections::HashMap;
+
             #[derive(PartialEq,Clone,Debug)]
             pub struct Node{
-                label: String,
-                attrs : Vec<(String, String)>
+                pub label : String,
+                pub attrs : HashMap<String, String>
             }
 
             impl Node {
                 pub fn new(label: &str) -> Self {
                     Node {
                         label : label.to_string(),
-                        attrs : vec![]
+                        attrs : HashMap::new()
                     }
                 }
 
-                pub fn with_attrs(mut self, attrs: &[(String, String)]) -> Self {
-                    self.attrs.clone_from_slice(attrs);
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    self.attrs = attrs.iter().map(|(key, value)| (key.to_string(), value.to_string())).collect();
                     self
+                }
+
+                pub fn get_attr(&self, key: &str) -> Option<&str> {
+                    self.attrs.get(key).map(|value| &**value)
                 }
             }
         }
